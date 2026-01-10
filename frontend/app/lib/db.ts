471056c57f02cfef57c966fd656b480bfefcd9ad
Connor Ketcheson <connorketcheson01@gmail.com>
2026-01-10 10:25:14 +0900

    change-db-connection

--- frontend/app/lib/db.ts
+++ frontend/app/lib/db.ts
@@ -1,13 +1,16 @@
 import pkg from "pg";
-
 const { Pool } = pkg;
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: Number(process.env.PG_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
